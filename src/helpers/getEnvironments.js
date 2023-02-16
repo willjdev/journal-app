e9e4468703960e72f7fbebe17f888916729183b7
William <william.jaimesfontalvo@gmail.com>
2023-02-16 14:48:45 -0500

    Variables de entorno modificadas

--- src/helpers/getEnvironments.js
+++ src/helpers/getEnvironments.js
@@ -2,9 +2,15 @@
 
 export const getEnvironments = () => {
 
-    import.meta.env; //Esto lo carga y ya adelante se puede usar en el return
+    //import.meta.env; //Esto lo carga y ya adelante se puede usar en el return
 
   return {
-    ...import.meta.env
+    //...import.meta.env
+    VITE_APIKEY: import.meta.env.VITE_APIKEY,
+    VITE_AUTHDOMAIN: import.meta.env.VITE_AUTHDOMAIN,
+    VITE_PROJECTID: import.meta.env.VITE_PROJECTID,
+    VITE_STORAGEBUCKET: import.meta.env.VITE_STORAGEBUCKET,
+    VITE_MESSAGINGSENDERID: import.meta.env.VITE_MESSAGINGSENDERID,
+    VITE_APPID: import.meta.env.VITE_APPID,
   }
 }
